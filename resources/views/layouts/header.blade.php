cb75224b78618367b0dd1680051db437364ab0dc
RaiyanKhalil <raiyankhalil@gmail.com>
2022-12-04 14:39:14 -0800

    simple map implementation

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,6 +6,7 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
 
   </head>
   <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,10 @@ if(Auth::user()) $isDoctor = Auth::user()->role_id == 2;
 
 <div class="container-fluid" style="width: 70%;">
 
+
+    <div id="map" style="width: 100%; height: 400px;"></div>
+
+    <br>
     <form action="{{ route('search') }}" method="GET">
         <input type="text" name="search" value="{{ request('search') }}" />
         <button type="submit">Search</button>
@@ -44,6 +48,45 @@ if(Auth::user()) $isDoctor = Auth::user()->role_id == 2;
     </div>
 </div>
 
+
+<script>
+     var attribution = new ol.control.Attribution({
+        collapsible: false
+    });
+
+    var map = new ol.Map({
+        controls: ol.control.defaults({attribution: false}).extend([attribution]),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM({
+                    url: 'https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png',
+                    attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>' ],
+                    maxZoom: 18
+                })
+            })
+        ],
+        target: 'map',
+        view: new ol.View({
+            center: ol.proj.fromLonLat([4.35247, 50.84673]),
+            maxZoom: 18,
+            zoom: 12
+        })
+    });
+
+
+
+    var layer = new ol.layer.Vector({
+     source: new ol.source.Vector({
+            features: [
+                new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([4.35247, 50.84673]))
+                })
+            ]
+        })
+    });
+    map.addLayer(layer);
+
+</script>
 @endsection
 @extends('layouts.footer')
 

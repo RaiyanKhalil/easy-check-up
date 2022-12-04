f9d85756023bb722a6c191d4e244b8305aa98c1d
drake7 <deepak.kumar7v@gmail.com>
2022-12-04 14:26:51 -0800

    loc api changes

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Client\ConnectionException;
 class LocationController extends Controller
 {
     //
-    public function getLongLat()
+    public static function getLongLat($address)
     {
         $response = Http::get('http://api.positionstack.com/v1/forward?access_key=1e6f8d4a91222bf72dcec5e91f02732a&query=1600 Pennsylvania Ave NW, Washington DC');
         // var_dump($ressponse) ;

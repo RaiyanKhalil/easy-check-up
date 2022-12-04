07974e696deed55d2958f5eda6efc96c823bfee1
drake7 <deepak.kumar7v@gmail.com>
2022-12-04 14:15:57 -0800

    Changes for the location

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -12,20 +12,21 @@ use Illuminate\Http\Client\ConnectionException;
 class LocationController extends Controller
 {
     //
-    public function getExternalDoctors()
+    public function getLongLat()
     {
-        $response = Http::get('http://api.positionstack.com/v1/forward
-        ? access_key = 1e6f8d4a91222bf72dcec5e91f02732a
-        & query = 1600 Pennsylvania Ave NW, Washington DC');
+        $response = Http::get('http://api.positionstack.com/v1/forward?access_key=1e6f8d4a91222bf72dcec5e91f02732a&query=1600 Pennsylvania Ave NW, Washington DC');
         // var_dump($ressponse) ;
         $foo =  json_decode($response->body())->data;
-        var_dump($foo);
-       /* $docArr = array(1,2,3);
-        foreach ($foo as $x => $d) { 
-            $docArr[$x] = $d->attributes;
-        }*/
+        //  var_dump($foo->latitude);
+        $locArr = array($foo[0]->latitude, $foo[0]->longitude);
 
-        return view('welcome')->with('docData');
+       /* foreach ($foo as $x => $d) { 
+            $docArr[$x] = $d->latitude;
+            $docArr[$x] = $d->latitude;
+            
+        }*/
+        var_dump($locArr);
+        return $locArr;
     }
 
 }
